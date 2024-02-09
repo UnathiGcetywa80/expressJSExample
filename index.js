346902bbe9f9d724617a054cfb87c95d16a70ea7
UnathiGcetywa80 <unathigcetywa80@gmail.com>
2024-02-09 16:18:30 +0200

    expressContent

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Welcome to ExpressJS");
+import express from 'express'
+// const express = require 'express'
+
+//create an express app
+const app = express()
+const port = +process.env.PORT || 4000
+
+//Router
+app.get('/', (req, res)=>{
+    res.json({
+        status: res.statusCode,
+        msg:'You`re home'
+    })
+
+})
+app.all('*', (req, res)=>{
+    res.json({
+        status: 404,
+        msg: '404 page'
+    })
+})
+
+ app.listen(port)
